bfaa18462b11d80f62495e91ca1f5d614c6b1b0f
Louis Kirkham <chew@talk21.com>
2020-11-29 17:36:19 +0000

    refactor paye calc

--- src/PayeCalc.js
+++ src/PayeCalc.js
@@ -1,16 +1,30 @@
 class PayeCalc {
 
+  constructor() {
+    this.payeThresholds = {"PA": 12500, "basic": 50000}
+    this.payeRates = {"basic": 0.20, "higher": 0.40}
+  }
+
   paye(gross) {
-    if(gross>50000) {
-      var higher = (gross - 50000) * 0.4
-      var basic = (50000-12500) * 0.2
-      return higher + basic
-    }
-    if(gross>12500) {
-      return (gross-12500) * 0.2
+    if(gross > this.payeThresholds["basic"]) {
+      return this.aboveBasic(gross) + this.basicBandTax()
+    } else if(gross > this.payeThresholds["PA"]) {
+      return this.aboveAllowance(gross)
     } else {
       return 0
     }
+  }
+
+  basicBandTax() {
+    return (this.payeThresholds["basic"] - this.payeThresholds["PA"]) * this.payeRates["basic"]
+  }
+
+  aboveAllowance(gross) {
+    return (gross - this.payeThresholds["PA"]) * this.payeRates["basic"]
+  }
+
+  aboveBasic(gross) {
+    return (gross - this.payeThresholds["basic"]) * this.payeRates["higher"]
   }
 
 
