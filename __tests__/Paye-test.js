cad09383929e88c5afdae29e0aa0fb289354ddbe
Louis Kirkham <chew@talk21.com>
2020-11-29 17:28:47 +0000

    added higher rate paye deduction

--- __tests__/Paye-test.js
+++ __tests__/Paye-test.js
@@ -5,10 +5,15 @@ import PayeCalc from '../src/PayeCalc'
 
 const calc = new PayeCalc()
 
-it('returns zero deduction under the threshold', () => {
+it('returns zero deduction under the personal allowance', () => {
   expect(calc.paye(5000)).toBe(0);
 });
 
 it('deducts tax at basic rate', () => {
   expect(calc.paye(25000)).toBe(2500)
 })
+
+it('deducts tax at higher rate', () => {
+  expect(calc.paye(55000)).toBe(9500)
+})
+
--- src/PayeCalc.js
+++ src/PayeCalc.js
@@ -1,6 +1,11 @@
 class PayeCalc {
 
   paye(gross) {
+    if(gross>50000) {
+      var higher = (gross - 50000) * 0.4
+      var basic = (50000-12500) * 0.2
+      return higher + basic
+    }
     if(gross>12500) {
       return (gross-12500) * 0.2
     } else {
