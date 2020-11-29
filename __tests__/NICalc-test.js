23019747bfef80a97348d5d7b9fdc778d41e89d7
Louis Kirkham <chew@talk21.com>
2020-11-29 15:32:54 +0000

    testing configured

---
+++ __tests__/NICalc-test.js
@@ -0,0 +1,7 @@
+const { clearUpdateCacheExperimentalAsync } = require("expo-updates");
+const { interpolate } = require("react-native-reanimated");
+
+// import NICalc from './src/NICalc'
+it('works', () => {
+  expect(1).toBe(1);
+});
