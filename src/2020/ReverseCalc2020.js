48c3a8c6061338e7e0290c1792ef559f33c8c064
Louis Kirkham <chew@talk21.com>
2020-12-01 09:36:23 +0000

    refactor

--- src/2020/ReverseCalc2020.js
+++ src/2020/ReverseCalc2020.js
@@ -8,16 +8,6 @@ class ReverseCalc2020 extends ReverseCalc{
     super()
     this.calc = new TakeHomeCalc2020()
   };
-
-  calculateSalaryFromMonthly(net) {
-    var gross = 0
-    var comparison = 0
-    while (comparison < net * 12) {
-      gross ++
-      comparison = this.calc.takeHome(gross)
-    }
-    return gross
-  };
 };
 
 export default ReverseCalc2020;
